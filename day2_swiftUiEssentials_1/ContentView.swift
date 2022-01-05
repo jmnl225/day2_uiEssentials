65b5da93852a5e7d3c5af44a57cdb9432e1e5a91
joomin <joominl15@gmail.com>
2022-01-06 00:06:52 +0900

    Initial Commit

---
+++ day2_swiftUiEssentials_1/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  day2_swiftUiEssentials_1
+//
+//  Created by joomin Lee on 6/1/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ day2_swiftUiEssentials_1/day2_swiftUiEssentials_1App.swift
@@ -0,0 +1,17 @@
+//
+//  day2_swiftUiEssentials_1App.swift
+//  day2_swiftUiEssentials_1
+//
+//  Created by joomin Lee on 6/1/22.
+//
+
+import SwiftUI
+
+@main
+struct day2_swiftUiEssentials_1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
